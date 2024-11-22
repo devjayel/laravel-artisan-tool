0c35aa9f8c5a49c15ae43a6f63fcbe67c8447ac4
JL Romero Juanitas <johnlenard.juanitas@yahoo.com>
2024-11-23 05:14:00 +0800

    Fix the electron:build

--- main.cjs
+++ main.cjs
@@ -8,7 +8,7 @@ app.on("ready", () => {
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, "preload.cjs"),
+            preload: path.join(__dirname, "preload.js"),
             nodeIntegration: true,
         },
     });
